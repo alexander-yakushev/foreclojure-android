912529f9ea7c13874ffaa37a015c62a5165223b5
Alexander Yakushev <alex@bytopia.org>
2015-08-21 14:06:48 +0300

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                        "Using empty encryption key."))
          "00000000000000000000000000000000")))
 
-(defproject foreclojure-android/foreclojure-android "0.1.1"
+(defproject foreclojure-android/foreclojure-android "0.2.0"
   :description "Android client for 4clojure.com"
   :url "https://github.com/alexander-yakushev/foreclojure-android"
   :license {:name "Eclipse Public License"
@@ -17,11 +17,12 @@
   :source-paths ["src/clojure" "src"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :plugins [[lein-droid "0.4.0-alpha3"]]
+  :plugins [[lein-droid "0.4.0"]]
 
-  :dependencies [[org.clojure-android/clojure "1.7.0-RC1" :use-resources true]
-                 [neko/neko "4.0.0-alpha1"]
-                 [org.clojure-android/data.json "0.2.6-SNAPSHOT"]]
+  :dependencies [[org.clojure-android/clojure "1.7.0-r2"]
+                 [neko/neko "4.0.0-alpha4"]
+                 [org.clojure-android/data.json "0.2.6-SNAPSHOT"]
+                 [com.android.support/design "22.2.1" :extension "aar"]]
   :profiles {:default [:dev]
 
              :dev
@@ -33,24 +34,18 @@
                          :manifest-options {:app-name "4Clojure - debug"}}}]
 
              :release
-             [:android-common
+             [:android-common :android-release
               {:target-path "target/release"
                :android {:ignore-log-priority [:debug :verbose]
                          :enable-dynamic-compilation true
                          :aot :all
-                         :build-type :release}}]
-
-             :pre-release
-             [:release
-              {:target-path "target/pre-release"
-               :android {:use-debug-keystore true}}]}
+                         :build-type :release}}]}
 
   :android {:dex-opts ["-JXmx4096M" "--incremental"]
-            ;; :force-dex-optimize true
             :build-config {"ENC_ALGORITHM" "Blowfish"
                            "ENC_KEY" #=(get-enc-key)}
             :manifest-options {:app-name "@string/app_name"}
-            :target-version "15"
+            :target-version "21"
             :aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"
                              "cljs-tooling.complete" "cljs-tooling.info"
                              "cljs-tooling.util.analysis" "cljs-tooling.util.misc"
